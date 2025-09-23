2b1cb30a7795174a93f452b8078525367e1ec1bf
simengaasland <simen5427@gmail.com>
2025-09-23 21:07:42 +0200

    Created init function

--- MOD550-task2/data_model.py
+++ MOD550-task2/data_model.py
@@ -2,6 +2,7 @@
 This module contains the DataModel class
 '''
 import sys
+import pandas as pd
 import fastf1 as f1
 
 sys.path.append('C:/Users/simen/Documents/GitHub/MOD550-F1-Project/MOD550-task1')
@@ -12,9 +13,6 @@ class DataModel:
     Fitting docstring for this class
     '''
     def __init__(self):
-        print('Init')
-
-    def testing(self):
         year = 2024
         #Collecting schedule for entire season, excluding testing sessions
         event_schedule = f1.get_event_schedule(year, include_testing = False)
@@ -36,8 +34,16 @@ class DataModel:
             df_fp_data['FastestLapRace'] = da.get_fastest_race_lap()
             list_of_data.append(df_fp_data)
 
-        return list_of_data
+        #Combining list of DataFrames into one DataFrame
+        self.data = pd.concat(list_of_data, ignore_index=True)
+
+        #Removes NaN values for set for drivers who has not completed a FP lap
+        self.data = self.data.dropna(subset=['FastestLap'])
+        self.data.to_csv('F1_data.csv', index=False)
+
+    def show_data(self):
+        print(self.data)
 
 
 cake = DataModel()
-print(cake.testing())
+print(cake.show_data())
