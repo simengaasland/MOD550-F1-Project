20aefec94625d0ffb4d0b9e34ad6173aa78a94b8
simengaasland <simen5427@gmail.com>
2025-09-30 15:34:25 +0200

    Added TO DO for task 3

--- MOD550-task1/data_aquisition.py
+++ MOD550-task1/data_aquisition.py
@@ -211,3 +211,45 @@ class DataAquisition:
         plt.xlabel("Time [s]")
         plt.ylabel("Probability")
         plt.show()
+
+'''
+FOR LATER:
+
+def __init__(self):
+        year = 2024
+        #Collecting schedule for entire season, excluding testing sessions
+        event_schedule = f1.get_event_schedule(year, include_testing = False)
+
+        #Removing sprint weekends as they do not have FP2 and FP3
+        conventinal_event_schedule = event_schedule[
+            event_schedule['EventFormat'] == 'conventional'
+            ]
+
+        #Getting a list of all gps (['Sakhir', 'Jeddah' ...])
+        gps = conventinal_event_schedule['Location'].tolist()
+
+        #Empty list to collect all the DataFrames
+        list_of_data = []
+
+        for _, gp in enumerate(gps):
+            da = DataAquisition(gp, year)
+            df_fp_data = da.get_fastest_laps()
+            df_fp_data['FastestLapRace'] = da.get_fastest_race_lap()
+            list_of_data.append(df_fp_data)
+
+        #Combining list of DataFrames into one DataFrame
+        self.data = pd.concat(list_of_data, ignore_index=True)
+
+        #Removes NaN values for set for drivers who has not completed a FP lap
+        self.data = self.data.dropna(subset=['FastestLap'])
+        #self.data.to_csv('F1_data.csv', index=False)
+
+    def lin_reg(self,gp, year):
+        model = LinearRegression()
+        model.fit(self.data['FastestLap', 'GP'], self.data['FastestLapRace'])
+        return  model.predict(DataAquisition(gp, year).get_fastest_laps()['FastestLap', 'GP'])
+
+
+cake = DataModel()
+print(cake.lin_reg('Monza', 2025))
+'''
