de28f1766da86e80a0d85327b5cdf7677fcdf2ba
simengaasland <simen5427@gmail.com>
2025-09-30 15:33:20 +0200

    2d_point_gen now generates point close to a set straight line

--- MOD550-task1/DataGenerator.py
+++ MOD550-task1/DataGenerator.py
@@ -3,62 +3,66 @@ import matplotlib.pyplot as plt
 import random as rnd
 import seaborn as sb
 
-class DataGenerator: 
+class DataGenerator:
     def __init__(self, n_points):
         self.n_points = n_points
 
     def random_2d_point_gen(self):
         '''
-        Generates a list of random points in the 
-        xy plane. The x- and y-values range from 
-        0 - 10. 
+        Generates a list of random points in the
+        xy plane. The x- and y-values range from
+        0 - 10.
 
         Input
         -------
-        n_points: int 
-            Amount of points (x, y) 
+        n_points: int
+            Amount of points (x, y)
 
         Returns
         -------
         list
             List of points (x, y)
         '''
-        
+
         rnd_list = []
 
         #Creates list with random points
         for i in range(self.n_points):
             x = round(rnd.random()*10, 2)
-            y = round(rnd.random()*10, 2)
+            y = round(rnd.random(), 2)
             rnd_list.append([x,y])
 
         return rnd_list
-        
+
     def get_xy_values(self, point_list):
         '''
-        Takes a list of point and generates 
+        Takes a list of point and generates
         two lists, one with x-values and the
         other one with y-values.
-        
+
         Input
         ------
-        point_list: list 
+        point_list: list
             A list of points in the xy plane
-            
+
         Output
         ------
-        list 
-            List of x-values 
-        list 
-            List of y-values 
+        list
+            List of x-values
+        list
+            List of y-values
         '''
         x_values = []
-        y_values = [] 
+        y_values = []
+        m = 2
+        c = 5
 
         #Gets all the x- and y-values and puts them i seperate lists
         for i in range(len(point_list)):
             x_values.append(point_list[i][0])
-            y_values.append(point_list[i][1])
+
+            noise = rnd.randrange(-1000,1000)*0.01
+            y_values.append((x_values[i]*m + c) + noise)
 
         return x_values, y_values
 
@@ -68,13 +72,13 @@ class DataGenerator:
 
         Input
         -----
-        point_list: list 
+        point_list: list
             A list of points in the xy plane
         bins: int
             Amount of bins in the histogram plot
         '''
         x_values, y_values = self.get_xy_values(point_list)
-        
+
         #Plot histogram for x-values
         plt.hist(x_values, bins = bins, color='blue', edgecolor='black')
         plt.xlabel("x-values")
@@ -95,11 +99,11 @@ class DataGenerator:
 
         Input
         -----
-        point_list: list 
+        point_list: list
             A list of points in the xy plane
         '''
         x_values, y_values = self.get_xy_values(point_list)
-        
+
         heatmap,*_ = np.histogram2d(x_values, y_values, bins=10)
 
         plt.imshow(heatmap.T, origin='lower', cmap = 'hot')
