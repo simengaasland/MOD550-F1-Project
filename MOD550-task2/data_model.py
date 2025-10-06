fb47d148f56592ba3d257029e8429dfadc1f8ca5
simengaasland <simen5427@gmail.com>
2025-10-06 10:25:57 +0200

    Added split_train_validation_test()

--- MOD550-task2/data_model.py
+++ MOD550-task2/data_model.py
@@ -4,6 +4,7 @@ This module contains the DataModel class
 import sys
 import pandas as pd
 import numpy as np
+import random
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 
@@ -63,6 +64,39 @@ class DataModel:
         plt.title(f'$ \hat y = {round(m,2)} \cdot x + {round(c,2)}$')
         plt.show()
 
+    def  split_train_validation_test(self):
+        '''
+        This function randomly shuffles the data
+        and splits it into train, validation and
+        test. Split is set as follows:
+        Train : 70%
+        Validation: 10%
+        Test: 20%
+        '''
+        #Shuffling the data randomly
+        random.shuffle(self.data)
+
+        #Finding total data point (could just use self.N)
+        len_data = len(self.data)
+
+        #Validation data is 10% of total data
+        valid_item = int(round(len_data * 0.1))
+
+        #Validation is 20% of total data
+        test_item = valid_item + int(round(len_data * 0.2))
+
+        #Rest is train data (70%)
+
+        #Splitting the data
+        valid_data = self.data[0 : valid_item]
+        test_data = self.data[valid_item : test_item]
+        train_data = self.data[test_item : ]
+
+        return train_data, valid_data, test_data
+
+
+
 
 class1 = DataModel(n_points = 100)
-class1.simple_linear_regression()
+#class1.simple_linear_regression()
+#train_data, valid_data, test_data = class1.split_train_validation_test()
