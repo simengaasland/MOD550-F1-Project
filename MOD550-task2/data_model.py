b67efe4ec69317804f1f56152b6ab6a8d85950b9
simengaasland <simen5427@gmail.com>
2025-10-07 16:00:30 +0200

    Added pylint changes

--- MOD550-task2/data_model.py
+++ MOD550-task2/data_model.py
@@ -1,10 +1,11 @@
 '''
 This module contains the DataModel class
 '''
+
 import sys
-import pandas as pd
-import numpy as np
 import random
+
+import numpy as np
 from matplotlib import pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -14,27 +15,28 @@ from sklearn.mixture import GaussianMixture
 
 
 sys.path.append('C:/Users/simen/Documents/GitHub/MOD550-F1-Project/MOD550-task1')
-from DataGenerator import DataGenerator
+from data_generator import DataGenerator
 
 class DataModel:
     '''
     Fitting docstring for this class
     '''
     def __init__(self,n_points):
-        self.N = n_points
-        self.dg = DataGenerator(self.N)
+        self.n = n_points
+        self.dg = DataGenerator(self.n)
         self.data = self.dg.random_2d_point_gen()
 
-
     def simple_linear_regression(self, statistic = False):
-
-        if statistic == True:
+        '''
+        Doc string
+        '''
+        if statistic is True:
             train_data = self.data
         else:
             train_data, self.valid_data, _ = self.split_train_validation_test()
 
         #Splits up data into x- and y-values
-        X_train, y_train = self.dg.get_xy_values(train_data)
+        x_train, y_train = self.dg.get_xy_values(train_data)
 
         #Calculates mean of x- and y-values
         sum_x = 0
@@ -42,21 +44,21 @@ class DataModel:
         mean_x = 0
         mean_y = 0
 
-        for _, x_val in enumerate(X_train):
+        for _, x_val in enumerate(x_train):
             sum_x += x_val
-        mean_x = sum_x / self.N
+        mean_x = sum_x / self.n
 
         for _, y_val in enumerate(y_train):
             sum_y += y_val
-        mean_y = sum_y / self.N
+        mean_y = sum_y / self.n
 
         #Calculate slope of regression line
         nominator_sum = 0
         denominator_sum = 0
 
         for i in range(len(train_data)):
-            nominator_sum += (X_train[i] - mean_x) * (y_train[i] - mean_y)
-            denominator_sum += (X_train[i] - mean_x)**2
+            nominator_sum += (x_train[i] - mean_x) * (y_train[i] - mean_y)
+            denominator_sum += (x_train[i] - mean_x)**2
 
         self.m = nominator_sum / denominator_sum
 
@@ -65,13 +67,13 @@ class DataModel:
 
         #Calculate y = m*x + c
         self.y_pred = []
-        for _, x in enumerate(X_train):
+        for _, x in enumerate(x_train):
             self.y_pred.append(self.m*x + self.c)
 
         #Plot
-        plt.scatter(X_train, y_train, color = 'blue', label = 'Training data')
-        plt.plot(X_train, self.y_pred, color = 'red', label = 'Prediction')
-        plt.title(f'$ \hat y = {round(self.m,2)} \cdot x + {round(self.c,2)}$')
+        plt.scatter(x_train, y_train, color = 'blue', label = 'Training data')
+        plt.plot(x_train, self.y_pred, color = 'red', label = 'Prediction')
+        plt.title(r'$\hat y = {round(self.m,2)} \cdot x + {round(self.c,2)}$')
         plt.show()
 
     def  split_train_validation_test(self):
@@ -104,7 +106,7 @@ class DataModel:
 
         return train_data, valid_data, test_data
 
-    def calc_MSE(self):
+    def calc_mse(self):
         '''
         This function uses validation data to calulate Mean squared Error.
         If a linear regression has not been preformed the fuction will
@@ -112,7 +114,7 @@ class DataModel:
         '''
         try:
             #Get x- and y-values
-            X_valid, y_valid = self.dg.get_xy_values(self.valid_data)
+            x_valid, y_valid = self.dg.get_xy_values(self.valid_data)
 
             #Initialize
             mse = 0
@@ -120,15 +122,16 @@ class DataModel:
 
             #Calculate error squared
             for i, y_real in enumerate(y_valid):
-                y_pred = self.m * X_valid[i] + self.c
+                y_pred = self.m * x_valid[i] + self.c
                 error_squared += (y_real - y_pred)**2
 
             #Calculate Mean Errro Squared and return
-            mse = (1/self.N) * error_squared
+            mse = (1/self.n) * error_squared
             return round(mse,2)
 
-        except:
+        except Exception as e:
             print('A linear regression has to be preformed before using calc_MSE function')
+            print(f'Error: {e}')
             mse = None
 
     def neural_network(self):
@@ -139,37 +142,38 @@ class DataModel:
         train_data, _ , test_data = self.split_train_validation_test()
 
         #Splits up data into x- and y-values
-        X_train, y_train = self.dg.get_xy_values(train_data)
-        X_test, y_test = self.dg.get_xy_values(test_data)
+        x_train, y_train = self.dg.get_xy_values(train_data)
+        x_test, y_test = self.dg.get_xy_values(test_data)
 
-        X_train = np.array(X_train)
+        x_train = np.array(x_train)
         y_train = np.array(y_train)
-        X_test = np.array(X_test)
+        x_test = np.array(x_test)
         y_test = np.array(y_test)
 
-        model = Sequential([Dense(32, input_dim = 1, activation = 'relu', kernel_regularizer =l2(0.01)),
-                            Dense(64, activation = 'relu', kernel_regularizer = l2(0.01)),
-                            Dense(64, activation = 'relu', kernel_regularizer = l2(0.01)),
-                            Dense(1)])
+        model = Sequential([
+            Dense(32, input_dim = 1, activation = 'relu', kernel_regularizer =l2(0.01)),
+            Dense(64, activation = 'relu', kernel_regularizer = l2(0.01)),
+            Dense(64, activation = 'relu', kernel_regularizer = l2(0.01)),
+            Dense(1)])
 
         model.compile(optimizer = 'Adam', loss = 'mean_squared_error')
 
-        model.fit(X_train,y_train, epochs = 70, verbose = 1)
+        model.fit(x_train,y_train, epochs = 70, verbose = 1)
 
-        y_pred = model.predict(X_test)
-        print(X_train.shape)
-        plt.scatter(X_train, y_train, color = 'blue', label = 'Training data')
-        plt.plot(X_test, y_pred, color = 'red', label = 'NN prediction')
+        y_pred = model.predict(x_test)
+        print(x_train.shape)
+        plt.scatter(x_train, y_train, color = 'blue', label = 'Training data')
+        plt.plot(x_test, y_pred, color = 'red', label = 'NN prediction')
         plt.legend()
         plt.show()
 
-    def K_mean(self, n_clusters, elbow_plot = True):
+    def k_mean(self, n_clusters, elbow_plot = True):
         '''
         Doc string
         '''
         x, y = self.dg.get_xy_values(self.data)
 
-        if elbow_plot == True:
+        if elbow_plot is True:
             inertias = []
 
             for i in range(1,11):
@@ -192,7 +196,7 @@ class DataModel:
         plt.title(f'K means with {n_clusters} clusters')
         plt.show()
 
-    def GMM(self, n_clusters):
+    def gmm(self, n_clusters):
         '''
         Doc string
         '''
@@ -207,13 +211,4 @@ class DataModel:
         plt.ylabel('y')
         plt.title(f'Gaussian Mixture Model (GMM) with {n_clusters} clusters')
         plt.show()
-
-
-class1 = DataModel(n_points = 100)
-#class1.simple_linear_regression()
-#train_data, valid_data, test_data = class1.split_train_validation_test()
-#print(class1.calc_MSE())
-#class1.neural_network()
-#class1.K_mean(n_clusters = 3, elbow_plot = False)
-class1.GMM(n_clusters = 3)
 
